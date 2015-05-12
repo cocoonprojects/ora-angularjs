84fa240856b827a41e7f5dd0e4638f1da9c017f3
Andrea Bandera <dottorbabba@gmail.com>
2015-05-12 23:09:20 +0200

    Add task detail

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,24 +1,64 @@
-var collaborationControllers = angular.module('collaborationControllers', []);
-
-collaborationControllers.controller('TaskListCtrl', ['$scope', 'Task',
-	function ($scope, Task) {
-		$scope.tasks = Task.query();
-		$scope.count = function($map) {
-			return Object.keys($map).length;
-		};
-		$scope.alertMsg = null;
-		$scope.$watch('currOrg', function(newValue, oldValue) {
-			if(newValue != undefined) {
-				console.log("CurrOrg changed: " + newValue.organization.id);
+angular.module('collaborationControllers', [])
+	.controller('TaskListCtrl', ['$scope', '$modal', '$log', 'Task',
+		function ($scope, $modal, $log, Task) {
+			$scope.tasks = Task.query();
+			$scope.alertMsg = null;
+			$scope.$watch('currOrg', function(newValue, oldValue) {
+				if(newValue != undefined) {
+					$log.debug("CurrOrg changed: " + newValue.organization.id);
+				}
+			});
+			$scope.count = function($map) {
+				return Object.keys($map).length;
+			};
+			$scope.deleteTask = function(id) {
+				Task.delete(id);
+			};
+			$scope.openNewTask = function() {
+				var modalInstance = $modal.open({
+					animation: true,
+					templateUrl: "partials/new-task.html"
+				});
+			};
+			$scope.openTaskDetail = function(task) {
+				var modalInstance = $modal.open({
+					animation: true,
+					templateUrl: "partials/task-detail.html",
+					controller: 'TaskDetailCtrl',
+					resolve: {
+						task: function() {
+							return task;
+						}
+					}
+				});
+			};
+			$scope.openEditTask = function(id) {
+				$scope.task = Task.query({ taskId: id });
+				var modalInstance = $modal.open({
+					templateUrl: "partials/task-detail.html",
+					controller: 'TaskDetailCtrl'
+				});
 			}
-		});
-		$scope.deleteTask = function(id) {
-			Task.delete(id);
-		};
-		$scope.editTask = function(id) {
-
-		}
-	}]);
+		}])
+	.controller('TaskDetailCtrl', ['$scope', '$modalInstance', '$log', 'task',
+		function ($scope, $modalInstance, $log, task) {
+			$scope.task = task;
+			$scope.daysAgo = function(when) {
+				var now = new Date();
+				var d = new Date(Date.parse(when));
+				return Math.round(Math.abs((now.getTime() - d.getTime()) / (1000 * 60 * 60 * 24)));
+			};
+			$scope.statusClass = function() {
+				switch ($scope.task.status) {
+					case 20:
+					case 30:
+					case 40:
+						return 'text-success';
+					default :
+						return 'text-muted';
+				}
+			};
+		}]);
 //
 //collaborationControllers.controller('TaskDetailCtrl', ['$scope', '$routeParams', 'Task',
 //	function($scope, $routeParams, Task) {
@@ -30,11 +70,11 @@ collaborationControllers.controller('TaskListCtrl', ['$scope', 'Task',
 
 var identityControllers = angular.module('identityControllers', []);
 
-identityControllers.controller('MyOrgsListCtrl', ['$scope', '$http',
-	function ($scope, $http) {
+identityControllers.controller('MyOrgsListCtrl', ['$scope', '$http', '$log',
+	function ($scope, $http, $log) {
 		$http.get('data/memberships.json').success(function(data) {
 			$scope.orgs = data;
-			console.log($scope.orgs._embedded['ora:organization-membership']);
+			$log.debug($scope.orgs._embedded['ora:organization-membership']);
 		});
 		$scope.currOrg = null;
 		$scope.currUser = { id: "34220c78-b054-4bd8-9a5c-70acc30d9ddc" };
