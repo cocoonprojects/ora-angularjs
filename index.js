679854ffb9b3325e23f5d6e2d8332f87e33b35f5
Andrea Bandera <dottorbabba@gmail.com>
2015-10-26 15:24:41 +0100

    Close #17

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 var express = require('express');
 var httpProxy = require('http-proxy');
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8000;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var backend_url = 'http://oraproject.carmati.it';
+
 var server = express();
-server.set('port', 8000);
+server.set('port', server_port);
 server.use(express.static(__dirname + '/public'));
 
 var apiProxy = httpProxy.createProxyServer();
 
 server.all("/api/*", function(req, res) {
 	req.url = req.url.substring(4);
-    apiProxy.web(req, res, { target: 'http://oraproject.carmati.it' }, function(e) {
+    apiProxy.web(req, res, { target: backend_url }, function(e) {
 		console.log(e);
 	});
     console.log("Request made to " + req.url);
 });
 
-server.listen(server.get('port'), function() {
-    console.log('Express server listening on port ' + server.get('port'));
+server.listen(server.get('port'), server_ip_address, function() {
+    console.log('Express server listening on ' + server_ip_address + ', port ' + server.get('port'));
 });
