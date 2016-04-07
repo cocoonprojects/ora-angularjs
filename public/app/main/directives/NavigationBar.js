264390b65fff162c32b448490cbb9d37a74f3333
francesco-strazzullo <me@francescostrazzullo.info>
2016-04-07 20:47:07 +0200

    using navigation bar to navigate app

--- public/app/main/directives/NavigationBar.js
+++ public/app/main/directives/NavigationBar.js
@@ -1,14 +1,17 @@
 (function() {
 	"use strict";
 
-	angular.module('app').directive('navigationBar',[function(){
+	var STATES = ['org.collaboration','org.organizationStatement','flow','org.decisions','org.people'];
+
+	angular.module('app').directive('navigationBar',['$state',function($state){
 			return {
 				restrict: 'E',
 				replace: true,
 				templateUrl: 'app/main/partials/navigationBar.html',
-				link: function($scope, element, attrs) {
-                    console.log('Link');
-				}
+                link: function($scope, element, attrs) {
+					$scope.selectedIndex = _.indexOf(STATES,$state.current.name);
+                    console.log($scope.selectedIndex);
+                }
 			};
 		}
 	]);
