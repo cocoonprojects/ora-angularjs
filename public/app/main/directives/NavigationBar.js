5b4e73eb1cc7b36f2c217b428e3a38b340bec8b8
francesco-strazzullo <me@francescostrazzullo.info>
2016-04-07 17:42:41 +0200

    first draft of navigation bar

---
+++ public/app/main/directives/NavigationBar.js
@@ -0,0 +1,15 @@
+(function() {
+	"use strict";
+
+	angular.module('app').directive('navigationBar',[function(){
+			return {
+				restrict: 'E',
+				replace: true,
+				templateUrl: 'app/main/partials/navigationBar.html',
+				link: function($scope, element, attrs) {
+                    console.log('Link');
+				}
+			};
+		}
+	]);
+}());
