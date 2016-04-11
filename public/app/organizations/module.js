fbc12333c018fc5ab6d02d76fd01145300c6c6a9
francesco-strazzullo <me@francescostrazzullo.info>
2016-04-11 16:52:20 +0200

    cosmetics

--- public/app/organizations/module.js
+++ public/app/organizations/module.js
@@ -1,21 +1,21 @@
 angular.module('app.organizations', [
-		'ui.router'
-	])
-	.config(['$stateProvider', '$urlRouterProvider',
-		function($stateProvider, $urlRouterProvider) {
-			$stateProvider
-				.state('organizations', {
-					url: '/organizations',
-					templateUrl: 'app/organizations/partials/organizations.html',
-					controller: 'OrganizationListController as ctrl'
-				})
-				.state('org.settings', {
-					url: '/settings',
-					templateUrl: 'app/organizations/partials/organization-settings.html',
-					controller: 'OrganizationSettingsController as ctrl',
-					data: {
-						pillarName: 'SETTINGS'
-					}
-				});
-		}
-	]);
+	'ui.router'
+])
+.config(['$stateProvider', '$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+		$stateProvider
+			.state('organizations', {
+				url: '/organizations',
+				templateUrl: 'app/organizations/partials/organizations.html',
+				controller: 'OrganizationListController as ctrl'
+			})
+			.state('org.settings', {
+				url: '/settings',
+				templateUrl: 'app/organizations/partials/organization-settings.html',
+				controller: 'OrganizationSettingsController as ctrl',
+				data: {
+					pillarName: 'SETTINGS'
+				}
+			});
+	}
+]);
