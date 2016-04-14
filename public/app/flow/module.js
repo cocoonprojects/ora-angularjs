a949749b27f4e5899fe5f92e6faf21cb36e03d08
francesco-strazzullo <me@francescostrazzullo.info>
2016-04-14 10:48:34 +0200

    moving to flow page also when no streams is provided

--- public/app/flow/module.js
+++ public/app/flow/module.js
@@ -11,6 +11,8 @@ angular.module('app.flow', ['ui.router'])
 							var deferred = $q.defer();
 							streamService.query(SelectedOrganizationId.get(),function(data){
 								deferred.resolve(_.values(data._embedded['ora:stream']));
+							},function(){
+								deferred.resolve([]);
 							});
 							return deferred.promise;
 						}]
