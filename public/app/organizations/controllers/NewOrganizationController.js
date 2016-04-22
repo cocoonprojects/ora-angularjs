3efc344a23d7f6987f3869dfcb2110666f0fa6cb
francesco-strazzullo <me@francescostrazzullo.info>
2016-04-22 10:39:54 +0200

    added new stream automatically when a new organization is created

--- public/app/organizations/controllers/NewOrganizationController.js
+++ public/app/organizations/controllers/NewOrganizationController.js
@@ -1,19 +1,32 @@
-function NewOrganizationController($scope, $log, $mdDialog, organizationService) {
-	$scope.organization = {};
-	this.cancel = function() {
-		$mdDialog.cancel();
-	};
-	this.submit = function() {
-		organizationService.save({}, $scope.organization, $mdDialog.hide, function(httpResponse) {
-			switch(httpResponse.status) {
-				case 400:
-					httpResponse.data.errors.forEach(function(error) {
-						$scope.form[error.field].$error.remote = error.message;
-					});
-					break;
-				default:
-					$log.warn(httpResponse);
-			}
-		});
-	};
+function NewOrganizationController(
+	$scope,
+	$log,
+	$mdDialog,
+	organizationService,
+	streamService) {
+		$scope.organization = {};
+		this.cancel = function() {
+			$mdDialog.cancel();
+		};
+
+		var onCompleteNewOrganization = function(org){
+			$mdDialog.hide();
+			streamService.save(org.id,{
+				subject:'Stream'
+			});
+		}
+
+		this.submit = function() {
+			organizationService.save({}, $scope.organization, onCompleteNewOrganization, function(httpResponse) {
+				switch(httpResponse.status) {
+					case 400:
+						httpResponse.data.errors.forEach(function(error) {
+							$scope.form[error.field].$error.remote = error.message;
+						});
+						break;
+					default:
+						$log.warn(httpResponse);
+				}
+			});
+		};
 }
