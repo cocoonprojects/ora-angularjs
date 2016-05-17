ddc2e45b4a40036502bd9b3eca10b13a8b03f59b
francesco-strazzullo <me@francescostrazzullo.info>
2016-05-17 17:27:16 +0200

    Added flow type label conversion

---
+++ public/app/flow/filters/FlowTypeLabel.js
@@ -0,0 +1,12 @@
+angular.module('app.flow')
+	.constant('FLOW_TYPES_LABEL', {
+		'VoteIdea': 'Vote Idea',
+		'VoteCompletedItem': 'Vote Completed Item',
+		'VoteCompletedItemVotingClosed': 'Completed Item Voting Closed',
+		'VoteCompletedItemReopened': 'Completed Item Reopened'
+	})
+	.filter('flowTypeLabel', ['FLOW_TYPES_LABEL', function(FLOW_TYPES_LABEL) {
+		return function(type) {
+			return FLOW_TYPES_LABEL[type] || type;
+		};
+	}]);
