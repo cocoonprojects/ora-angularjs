13934acd4d3dc135e83b362058cbee93ae0e10ec
francesco-strazzullo <me@francescostrazzullo.info>
2016-05-21 15:18:25 +0200

    removed settings menu

--- public/app/main/directives/LeftMenu.js
+++ public/app/main/directives/LeftMenu.js
@@ -2,13 +2,23 @@
     "use strict";
 
     angular.module('app').directive('leftMenu',[
-        function(){
+        'SelectedOrganizationId',
+        function(
+            SelectedOrganizationId
+        ){
             return {
                 restrict: 'E',
                 replace: true,
                 templateUrl: 'app/main/partials/leftMenu.html',
                 link: function($scope, element, attrs) {
-                    
+
+                    $scope.organization = null;
+
+                    $scope.$on('$stateChangeSuccess',function(){
+                        if(SelectedOrganizationId.get()){
+                            $scope.organization = $scope.identity.getMembership(SelectedOrganizationId.get());
+                        }
+        			});
                 }
             };
         }
