7ad90e5fb5cdb126db359071b188a37e797eb351
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2016-05-23 16:06:51 +0200

    added border colored to item list cards

---
+++ public/app/collaboration/directives/ItemCardColor.js
@@ -0,0 +1,32 @@
+(function() {
+  "use strict";
+
+  angular.module('app')
+    .constant('TASK_STATUS_COLORS', {
+      0: '#ebeef0',
+      10: '#bec9d0',
+      20: '#9eaeb6',
+      30: '#44515a',
+      40: '#1e2c36',
+      50: '#050e14',
+      '-20': '#050e14'
+    })
+    .directive('itemCardColor', [
+      "TASK_STATUS_COLORS",
+      function(TASK_STATUS_COLORS) {
+        return {
+          restrict: 'A',
+          scope: {
+            card: '='
+          },
+          link: function($scope, element, attrs) {
+            $scope.$watch('card', function(card) {
+              element.parent('md-card').css('border-left-color',TASK_STATUS_COLORS[card.status]);
+            });
+          }
+
+
+        };
+      }
+    ]);
+}());
