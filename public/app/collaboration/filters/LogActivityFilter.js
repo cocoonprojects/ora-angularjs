76a361116930ecfac97bb9a2eebe8204e7fa1e85
francesco-strazzullo <me@francescostrazzullo.info>
2016-05-30 15:48:33 +0200

    Fixed task name in log activity

--- public/app/collaboration/filters/LogActivityFilter.js
+++ public/app/collaboration/filters/LogActivityFilter.js
@@ -2,6 +2,7 @@ angular.module('app.collaboration')
 .filter('logActivityFilter', [function() {
 	return function(historyElement) {
         var date = moment(historyElement.on,"DD/MM/YYYY HH:mm:SS").format('DD/MM/YYYY');
-        return date + " - " + historyElement.name;
+        var name = historyElement.name.replace(/([A-Z])/g, ' $1');
+        return date + " - " + name;
 	};
 }]);
