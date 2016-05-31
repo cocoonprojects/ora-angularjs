e3eacf32317db035e21aaa746583d5e33c14ff60
francesco-strazzullo <me@francescostrazzullo.info>
2016-05-31 17:24:59 +0200

    created invitation route

---
+++ public/app/organizations/controllers/ConfirmInvitationController.js
@@ -0,0 +1,13 @@
+angular.module('app')
+	.controller('ConfirmInvitationController', [
+		'$scope',
+		'$stateParams',
+		'$state',
+		'$mdDialog',
+		function (
+			$scope,
+			$stateParams,
+			$state,
+			$mdDialog) {
+
+		}]);
--- public/app/organizations/module.js
+++ public/app/organizations/module.js
@@ -9,6 +9,11 @@ angular.module('app.organizations', [
 				templateUrl: 'app/organizations/partials/organizations.html',
 				controller: 'OrganizationListController as ctrl'
 			})
+			.state('invitation', {
+				url: '/organizations/acceptinvite?token',
+				templateUrl: 'app/organizations/partials/confirmInvitation.html',
+				controller: 'ConfirmInvitationController'
+			})
 			.state('org.settings', {
 				url: '/settings',
 				templateUrl: 'app/organizations/partials/organization-settings.html',
