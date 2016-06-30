fc679d84e2cc6160831bb33ce3fa178399158672
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2016-06-30 12:36:16 +0200

    new layout for navigation bar visbilty

--- public/app/main/controllers/BaseOrganizationController.js
+++ public/app/main/controllers/BaseOrganizationController.js
@@ -15,10 +15,20 @@ angular.module('app')
             streams,
 			SelectedOrganizationId,
 			$state) {
-				if(!SelectedOrganizationId.get()){
-					$state.go("organizations");
-					return;
-				}
+
+                var STATES = ['org.collaboration','org.organizationStatement','org.flow','org.decisions','org.people'];
+                var MINORSTATES = {
+                    "org.item":"org.collaboration"
+                };
+
+                var checkSelectedStateIndex = function(currentState) {
+                    //currentState = MINORSTATES[currentState] || currentState;
+                    return _.indexOf(STATES,currentState);
+                };
+    			if(!SelectedOrganizationId.get()){
+    				$state.go("organizations");
+    				return;
+    			}
 
 				$scope.organization = $scope.identity.getMembership($stateParams.orgId);
                 $scope.members = members;
@@ -50,6 +60,33 @@ angular.module('app')
 						if(toState.data){
                             $scope.fullHeight = toState.data.fullHeight;
                         }
+                    }
+                );
+                var selectedOrganizationId = SelectedOrganizationId.get();
+                    if(selectedOrganizationId){
+                        $scope.organizationId = selectedOrganizationId;
+                    }else{
+                        $scope.organizationId = null;
+                        identity.loadMemberships().then(function(memberships){
+                            if(memberships && memberships.length){
+                                $scope.organizationId = memberships[0].organization.id;
+                            }
+                    });
+                }
+
+                $scope.selectedIndex = checkSelectedStateIndex($state.current.name);
+
+                $scope.$on('$stateChangeSuccess',
+                    function(event, toState) {
+                        $scope.selectedIndex = checkSelectedStateIndex(toState.name);
+                    }
+                );
+
+                $scope.selectedIndex = checkSelectedStateIndex($state.current.name);
+
+                $scope.$on('$stateChangeSuccess',
+                    function(event, toState) {
+                        $scope.selectedIndex = checkSelectedStateIndex(toState.name);
                     }
                 );
             }
--- public/app/main/directives/NavigationBar.js
+++ public/app/main/directives/NavigationBar.js
@@ -1,52 +1,16 @@
 (function() {
-	"use strict";
+    "use strict";
 
-	var STATES = ['org.collaboration','org.organizationStatement','org.flow','org.decisions','org.people'];
-	var MINORSTATES = {
-		"org.item":"org.collaboration"
-	};
-
-	var checkSelectedStateIndex = function(currentState) {
-		//currentState = MINORSTATES[currentState] || currentState;
-		return _.indexOf(STATES,currentState);
-	};
-
-	angular.module('app').directive('navigationBar',[
-			'$state',
-			'identity',
-			'SelectedOrganizationId',
-			function(
-				$state,
-				identity,
-				SelectedOrganizationId){
-			return {
-				restrict: 'E',
-				replace: true,
-				templateUrl: 'app/main/partials/navigationBar.html',
+    angular.module('app').directive('navigationBar',
+        function() {
+            return {
+                restrict: 'E',
+                replace: true,
+                templateUrl: 'app/main/partials/navigationBar.html',
                 link: function($scope, element, attrs) {
-					var selectedOrganizationId = SelectedOrganizationId.get();
-					if(selectedOrganizationId){
-						$scope.organizationId = selectedOrganizationId;
-					}else{
-						$scope.organizationId = null;
-						identity.loadMemberships().then(function(memberships){
-							if(memberships && memberships.length){
-								$scope.organizationId = memberships[0].organization.id;
-							}
-						});
-					}
-
-					$scope.selectedIndex = checkSelectedStateIndex($state.current.name);
-
-					$scope.$on('$stateChangeSuccess',
-	                    function(event, toState) {
-                        	$scope.selectedIndex = checkSelectedStateIndex(toState.name);
-	                    }
-	                );
-
 
                 }
-			};
-		}
-	]);
+            };
+        }
+    );
 }());
